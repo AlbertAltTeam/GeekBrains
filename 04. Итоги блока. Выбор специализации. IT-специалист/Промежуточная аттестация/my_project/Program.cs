cc38594951ec99086ac84f377944f1751e1f3ad9
AlbertAltTeam <104348456+AlbertAltTeam@users.noreply.github.com>
2023-04-03 12:14:15 +0400

    04. Итоги блока. Выбор специализации

---
+++ 04. Итоги блока. Выбор специализации. IT-специалист/Промежуточная аттестация/my_project/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        // Вводим исходный массив с клавиатуры
+        Console.Write("Введите элементы массива через запятую: ");
+        string inputString = Console.ReadLine();
+        string[] inputArray = inputString.Split(',');
+
+        // Создаем новый массив из строк, длина которых меньше или равна 3 символам
+        string[] outputArray = new string[inputArray.Length];
+        int j = 0; // Индекс текущего элемента в выходном массиве
+        for (int i = 0; i < inputArray.Length; i++) {
+            if (!string.IsNullOrEmpty(inputArray[i]) && inputArray[i].Length <= 3) {
+                outputArray[j] = inputArray[i];
+                j++;
+            }
+        }
+
+        // Если новый массив пустой - выводим сообщение об этом
+        if (j == 0) {
+            Console.WriteLine("Нет строк длиной меньше или равной 3 символам.");
+            return; // Выходим из метода Main, так как больше делать нечего
+        }
+
+        // Обрезаем выходной массив до размера j (т.е. количества отфильтрованных строк)
+        Array.Resize(ref outputArray, j);
+
+        // Выводим результат на консоль
+        Console.WriteLine("Отфильтрованный массив: {0}", string.Join(", ", outputArray));
+    }
+}
