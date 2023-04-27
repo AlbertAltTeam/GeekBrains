e28b91447ebd63f9268f52f68f955a3a6cde16da
AlbertAltTeam <104348456+AlbertAltTeam@users.noreply.github.com>
2023-04-27 11:37:57 +0400

    small fix

--- 07. JavaScript (семинары)/Урок 3. Знакомство с функциями в JavaScript/Домашняя работа/Задание4.js
+++ 07. JavaScript (семинары)/Урок 3. Знакомство с функциями в JavaScript/Домашняя работа/Задание4.js
@@ -2,8 +2,8 @@ function sum(a, b) {
     return a + b;
 }
 
-function subtract(a, b) {
-    return a > b ? a - b : 0;
+function difference(a, b) {
+    return a === b ? 0 : a > b ? a - b : b - a;
 }
 
 function multiply(a, b) {
@@ -11,11 +11,15 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    return b === 0 ? 0 : a / b;
 }
 
 
+
 console.log(sum(2, 6)); // 8
-console.log(subtract(10, 3)); // 7
-console.log(multiply(4, 5)); // 20
-console.log(divide(15, 3)); // 5
+console.log(difference(3, 3)); // 0
+console.log(difference(5, 3)); // 2
+console.log(difference(10, 20)); // 10
+console.log(multiply(2, 6)); // 12
+console.log(divide(6, 2)); // 3
+console.log(divide(6, 0)); // 0
