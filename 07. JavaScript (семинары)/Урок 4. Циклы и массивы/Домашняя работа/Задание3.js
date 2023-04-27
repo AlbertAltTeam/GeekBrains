49a56cdf43b9ecb3be78afc937b2e33f62f8cc78
AlbertAltTeam <104348456+AlbertAltTeam@users.noreply.github.com>
2023-04-27 06:32:06 +0400

    added one more solution

--- 07. JavaScript (семинары)/Урок 4. Циклы и массивы/Домашняя работа/Задание3.js
+++ 07. JavaScript (семинары)/Урок 4. Циклы и массивы/Домашняя работа/Задание3.js
@@ -1,15 +1,26 @@
+//Вариант 1
+
 const arr = [];
 for (let i = 0; i < 5; i++) {
   arr.push(Math.floor(Math.random() * 10));
 }
 
 const sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue);
-
 const min = Math.min(...arr);
-
 const containsThree = arr.includes(3);
 
-console.log(`Массив: ${arr}`);
-console.log(`Сумма элементов массива: ${sum}`);
-console.log(`Минимальное число в массиве: ${min}`);
-containsThree ? console.log("Число 3 есть в массиве") : console.log("Число 3 отсутствует в массиве");
+console.log("Массив:", arr);
+console.log("Сумма элементов массива:", sum);
+console.log("Минимальное число в массиве:", min);
+containsThree ?? console.log("Число 3 есть в массиве");
+
+//Вариант 2
+const arr2 = Array.from({length: 5}, () => Math.floor(Math.random() * 10));
+const sum2 = arr2.reduce((total, number) => total + number);
+const minNumber = Math.min.apply(null, arr2);
+const hasThree = arr2.indexOf(3) !== -1;
+
+console.log(`Массив: ${arr2}`);
+console.log(`Сумма элементов массива: ${sum2}`);
+console.log(`Минимальное число в массиве: ${minNumber}`);
+console.log(`Число 3 ${hasThree ? 'найдено' : 'не найдено'} в массиве`);
